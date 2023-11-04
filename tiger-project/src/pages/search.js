339a980ff9e9c9414ed2e3ac7a52d02dab1f7250
Moe Alam <ma3655@rit.edu>
2023-11-04 19:18:27 -0400

    fb interaction setup

--- tiger-project/src/pages/search.js
+++ tiger-project/src/pages/search.js
@@ -1,7 +1,26 @@
 import Head from 'next/head';
+import { useState } from 'react';
 import styles from '../styles/Search.module.css'; // Assume you have a CSS module with your styles
 
 export default function Search() {
+    // State to store the input value
+    const [searchTerm, setSearchTerm] = useState('');
+
+    // Function to handle the button click and make the GET request
+    const handleSearchClick = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/reviewed/0');
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            console.log(data); // Printing the result in the console
+            // You can also set the data to state and display it in the component if needed
+        } catch (error) {
+            console.error('There was an error!', error);
+        }
+    };
+
     return (
 
         <>
@@ -19,8 +38,14 @@ export default function Search() {
 
                 <main className={styles.main}>
                     <div className={styles.searchBar}>
-                        <input type="text" placeholder="Enter your search term" />
-                        <button type="submit">🔍</button>
+                        <input
+                            type="text"
+                            placeholder="Enter your search term"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                        />
+                        {/* Attach the handleSearchClick function to the onClick event */}
+                        <button type="button" onClick={handleSearchClick}>🔍</button>
                     </div>
 
                     <div className={styles.accordion}>
